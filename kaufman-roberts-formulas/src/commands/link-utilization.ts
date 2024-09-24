2d3eab6a301c484aabb80e4851a17c3fb2891869
mariakourtesi <mkourtesi@hotmail.com>
2024-09-24 07:09:39 +0100

    feat: add trunk efficiency

--- kaufman-roberts-formulas/src/commands/link-utilization.ts
+++ kaufman-roberts-formulas/src/commands/link-utilization.ts
@@ -1,14 +1,22 @@
 import { ServiceClass } from '../types';
+import { numberOfDigitsAfterDecimal } from '../utils';
 import { kaufmanRoberts } from './kaufman-roberts-formula';
 
-export const linkUtilization_U = (capacity: number, serviceClasses: ServiceClass[]): string => {
+const calculateLinkUtilization = (capacity: number, serviceClasses: ServiceClass[]): number => {
   const probabilities = kaufmanRoberts(capacity, serviceClasses);
 
-  const result = Array.from({ length: capacity }, (_, j) => {
+  return Array.from({ length: capacity }, (_, j) => {
     const q_j = probabilities[`q(${j + 1})`];
-    console.log(q_j);
     return (j + 1) * q_j;
   }).reduce((sum, value) => sum + value, 0);
+};
 
-  return `${result.toFixed(7)} b.u`;
+export const linkUtilization_U = (capacity: number, serviceClasses: ServiceClass[]): string => {
+  const linkUtilization = calculateLinkUtilization(capacity, serviceClasses);
+  return `${linkUtilization.toFixed(7)} b.u`;
+};
+
+export const trunkEficiency_n = (capacity: number, serviceClasses: ServiceClass[]): string => {
+  const utilization_G = calculateLinkUtilization(capacity, serviceClasses);
+  return `${((utilization_G / capacity) * 100).toFixed(numberOfDigitsAfterDecimal)}%`;
 };
