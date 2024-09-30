040d700a5cf2ecb2ca14da282182a70799c2ef1b
mariakourtesi <mkourtesi@hotmail.com>
2024-09-30 21:44:26 +0100

    chore: update CBP function to calculate call blocking probability in BR policy

--- kaufman-roberts-formulas/src/commands/call-blocking-probability.ts
+++ kaufman-roberts-formulas/src/commands/call-blocking-probability.ts
@@ -1,15 +1,19 @@
-import { ServiceClass } from '../types';
+import { ServiceClass, ServiceClassWithBR } from '../types';
 import { kaufmanRoberts } from './kaufman-roberts-formula';
 
-export const callBlockingProbability = (capacity: number, serviceClasses: ServiceClass[]) => {
+export const callBlockingProbability = (
+  capacity: number,
+  serviceClasses: ServiceClass[] | ServiceClassWithBR[]
+) => {
   const probabilityValues = kaufmanRoberts(capacity, serviceClasses);
   const result: { [key: string]: string } = {};
 
   serviceClasses.forEach((serviceClass, index) => {
     const requested_bu = serviceClass.bu;
+    const tk = (serviceClass as ServiceClassWithBR).tk || 0;
     let cbp = 0;
 
-    for (let j = capacity - requested_bu + 1; j <= capacity; j++) {
+    for (let j = capacity - requested_bu - tk + 1; j <= capacity; j++) {
       const q_j = probabilityValues[`q(${j})`] || 0;
       cbp += q_j;
     }
