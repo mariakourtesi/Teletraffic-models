7bc2dd88d576d49d5677b3b4b6fdbe28f97b6ff9
mariakourtesi <mkourtesi@hotmail.com>
2024-09-30 21:14:40 +0100

    chore: refactor and move shared functions to utils

--- kaufman-roberts-formulas/src/commands/kaufman-roberts-formula.ts
+++ kaufman-roberts-formulas/src/commands/kaufman-roberts-formula.ts
@@ -1,5 +1,27 @@
 import { ServiceClass } from '../types';
-import { numberOfDigitsAfterDecimal } from '../utils';
+import { normaliseProbabilityValues, numberOfDigitsAfterDecimal } from '../utils';
+
+const blockingProbablity_q = (j: number, serviceClasses: ServiceClass[]): number => {
+  const results: number[] = [];
+  // Base case: q(0) = 1
+  if (j === 0) {
+    results[0] = 1;
+    return 1;
+  }
+  if (j < 0) return 0;
+
+  let sum = 0;
+  for (const serviceClass of serviceClasses) {
+    const { bu, incomingLoad_a } = serviceClass;
+    sum += incomingLoad_a * bu * blockingProbablity_q(j - bu, serviceClasses);
+  }
+
+  const result = (1 / j) * sum;
+
+  results[j] = parseFloat(result.toFixed(numberOfDigitsAfterDecimal));
+
+  return result;
+};
 
 export const unnormalisedKaufmanRobertsFormula = (
   capacity: number,
@@ -9,44 +31,13 @@ export const unnormalisedKaufmanRobertsFormula = (
 
   const results: number[] = [];
 
-  const q = (j: number): number => {
-    // Base case: q(0) = 1
-    if (j === 0) {
-      results[0] = 1;
-      return 1;
-    }
-    if (j < 0) return 0;
-
-    let sum = 0;
-    for (const serviceClass of serviceClasses) {
-      const { bu, incomingLoad_a } = serviceClass;
-      sum += incomingLoad_a * bu * q(j - bu);
-    }
-
-    const result = (1 / j) * sum;
-
-    results[j] = parseFloat(result.toFixed(numberOfDigitsAfterDecimal));
-
-    return result;
-  };
-
   for (let j = 0; j <= capacity; j++) {
-    q(j);
+    results[j] = parseFloat(
+      blockingProbablity_q(j, serviceClasses).toFixed(numberOfDigitsAfterDecimal)
+    );
   }
 
   return results;
-};
-
-export const calculateNormalizationConstant_G = (unormalizedProbabilities: number[]): number => {
-  if (unormalizedProbabilities.length === 0) return 0;
-  return unormalizedProbabilities.reduce((acc, curr) => acc + curr, 0);
-};
-
-export const normaliseProbabilityValues = (probabilities: number[]): number[] => {
-  const sum = calculateNormalizationConstant_G(probabilities);
-  return probabilities.map((probability) =>
-    parseFloat((probability / sum).toFixed(numberOfDigitsAfterDecimal))
-  );
 };
 
 export const kaufmanRoberts = (capacity: number, serviceClasses: ServiceClass[]) => {
--- kaufman-roberts-formulas/src/utils.ts
+++ kaufman-roberts-formulas/src/utils.ts
@@ -1,1 +1,13 @@
 export const numberOfDigitsAfterDecimal = 7;
+
+export const calculateNormalizationConstant_G = (unormalizedProbabilities: number[]): number => {
+  if (unormalizedProbabilities.length === 0) return 0;
+  return unormalizedProbabilities.reduce((acc, curr) => acc + curr, 0);
+};
+
+export const normaliseProbabilityValues = (probabilities: number[]): number[] => {
+  const sum = calculateNormalizationConstant_G(probabilities);
+  return probabilities.map((probability) =>
+    parseFloat((probability / sum).toFixed(numberOfDigitsAfterDecimal))
+  );
+};
