5abb6da8bf0df16df5134c4fd661bedaedc30c0e
mariakourtesi <mkourtesi@hotmail.com>
2024-11-12 08:13:57 +0000

    chore: update variable names

--- kaufman-roberts-dziong-erlang-formulas/src/utils/binomial-distribution.ts
+++ kaufman-roberts-dziong-erlang-formulas/src/utils/binomial-distribution.ts
@@ -1,18 +1,18 @@
 import { factorial } from './factorial';
 
-const calculateBinomialCoefficient = (numOfTrials: number, numberOfSuccesses: number): number => {
-  return factorial(numOfTrials) / (factorial(numberOfSuccesses) * factorial(numOfTrials - numberOfSuccesses));
+const binomialCoefficient = (nTotalItems: number, kSelectedItems: number): number => {
+  return factorial(nTotalItems) / (factorial(kSelectedItems) * factorial(nTotalItems - kSelectedItems));
 };
 
-const binomialDistribution = (numOfTrials: number, probability: number, numberOfSuccesses: number): number => {
-  return calculateBinomialCoefficient(numOfTrials, numberOfSuccesses) * Math.pow(probability, numberOfSuccesses) * Math.pow(1 - probability, numOfTrials - numberOfSuccesses);
+const binomialDistribution = (nTotalItems: number, probability: number, kSelectedItems: number): number => {
+  return binomialCoefficient(nTotalItems, kSelectedItems) * Math.pow(probability, kSelectedItems) * Math.pow(1 - probability, nTotalItems - kSelectedItems);
 };
 
-export const binomialDistributionRangeProbability = (numOfTrials: number, probability: number, minSuccesses: number, maxSuccesses: number): number => {
+export const binomialDistributionRangeProbability = (nTotalItems: number, probability: number, minSuccesses: number, maxSuccesses: number): number => {
   let sum = 0;
 
   for (let i = minSuccesses; i <= maxSuccesses; i++) {
-    sum += binomialDistribution(numOfTrials, probability, i);
+    sum += binomialDistribution(nTotalItems, probability, i);
 
   }
   return sum;
