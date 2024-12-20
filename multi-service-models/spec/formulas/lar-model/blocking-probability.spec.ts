f247d51193c87b7a71a7db67933963471962b1da
mariakourtesi <mkourtesi@hotmail.com>
2024-12-20 05:57:27 +0000

    chore: add more tests in lar cbp

--- multi-service-models/spec/formulas/lar-model/blocking-probability.spec.ts
+++ multi-service-models/spec/formulas/lar-model/blocking-probability.spec.ts
@@ -30,6 +30,24 @@ describe('blocking probability', () => {
           E_class_1: '0.0036756',
           E_class_2: '0.0203967'
         }  
+      },
+      {
+        description: '1 service class, 1 bu',
+        distinctResourceCount: 1,
+        individualResourceCapacity: 5,
+        serviceClasses: [{ serviceClass: 1, bu:1, incomingLoad_a: 2 }],
+        expectedBlocking: {
+          E_class_1: '0.0366972'
+        }  
+      },
+      {
+        description: '1 service class, 2 bu',
+        distinctResourceCount: 1,
+        individualResourceCapacity: 5,
+        serviceClasses: [{ serviceClass: 1, bu:2, incomingLoad_a: 2 }],
+        expectedBlocking: {
+          E_class_1: '0.4000000'
+        }  
       }
     ])('when $description', ({distinctResourceCount, individualResourceCapacity, serviceClasses, expectedBlocking}) => {
       it('should calculate correctly the blocking probability for the system', () => {
--- multi-service-models/src/formulas/lar-model/blocking-probability.ts
+++ multi-service-models/src/formulas/lar-model/blocking-probability.ts
@@ -27,4 +27,3 @@ export const blockingProbabilityLAR = (
     });
     return result;
   }
-
